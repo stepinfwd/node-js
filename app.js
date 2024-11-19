b91ea6bb94db6cea43ae14a1abddfdbbe2a1dace
Kiran <kiran@b-link.co@gmail.com>
2024-11-19 16:31:15 +0530

    feat : added templating engine

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const ejs = require('ejs')
+
+
 const port = 8082
 const app = express()
 
-app.get('/', function (req, res) {
-    res.send('Basic server')
+console.log("__dirname---", __dirname);
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'Hey', message: 'Hello there!' })
+})
+
+app.get('/confirm', (req, res) => {
+    res.render('confirm', { title: 'Hey', message: 'Hello there!' })
+})
+
+app.get('/recommend', (req, res) => {
+    res.render('recommend', { title: 'Hey', message: 'Hello there!' })
+})
+
+app.get('/restaurants', (req, res) => {
+    res.render('restaurants', { title: 'Hey', message: 'Hello there!' })
 })
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
