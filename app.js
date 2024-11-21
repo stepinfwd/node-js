236ba0c6562c66b712f667d35eb516fd2fc0bfa0
Kiran <kiran@b-link.co@gmail.com>
2024-11-21 14:35:46 +0530

    feat : added error pages , completed restaurant detail page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 const ejs = require('ejs')
+const { v4: uuidv4 } = require('uuid');
 
 
 const port = 8082
@@ -32,8 +33,18 @@ app.get('/recommend', (req, res) => {
 
 app.get('/restaurants/:id', (req, res) => {
     const id = req.params.id
+    const filePath = path.join(__dirname, 'data', 'restaurants.json')
+    const restaurants = JSON.parse(fs.readFileSync(filePath))
+
+    for (restaurant of restaurants) {
+        console.log("restaurant---", restaurant.id, id);
+        if (restaurant.id == id)
+            return res.render('restaurant-details', { restaurantId: id, restaurant: restaurant })
+    }
+    res.status(404).render('404', { title: '404 - Page Not Found' });
+
+
     console.log("id---", id);
-    res.render('restaurant-details', { restaurantId: id, message: 'Hello there!' })
 })
 
 app.get('/restaurants', (req, res) => {
@@ -47,10 +58,25 @@ app.post('/recommend', (req, res) => {
     const restaurants = req.body
     const filePath = path.join(__dirname, 'data', 'restaurants.json')
     const data = JSON.parse(fs.readFileSync(filePath))
+    restaurants.id = uuidv4()
     data.push(restaurants)
     fs.writeFileSync(filePath, JSON.stringify(data))
     res.redirect('confirm')
 })
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    res.status(404).render('404', { title: '404 - Page Not Found' });
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    console.error(err.stack); // Log the error stack for debugging
+    res.status(err.status || 500).render('500', {
+        title: '500 - Internal Server Error',
+        message: 'Something went wrong!'
+    });
+});
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
